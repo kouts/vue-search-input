448c1a751f3899d6b5ce72f68133d7a15d1549dd
kouts <koutsaftakis@gmail.com>
2022-01-28 17:46:21 +0200

    test: restrict babel config to test env

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current'
-        }
-      }
-    ]
-  ]
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current'
+            }
+          }
+        ]
+      ]
+    }
+  }
 }
