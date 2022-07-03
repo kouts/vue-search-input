ba1b8763a35263c988d61f28093e45ab854eb6bb
Kouts <koutsaftakis@gmail.com>
2022-07-03 12:05:18 +0300

    fix: updated jest to v28

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,9 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
   moduleFileExtensions: ['vue', 'js', 'ts'],
   moduleNameMapper: {
+    // Fix in order for vue-test-utils to work with Jest 28
+    // https://github.com/vuejs/vue-test-utils/issues/1975
+    '^@vue/test-utils$': require.resolve('@vue/test-utils'),
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@playground/(.*)$': '<rootDir>/playground/$1',
     '^@root/(.*)$': '<rootDir>/$1'
