4480cc2236657f54a01952b4299d3d0a70f706c3
kouts <koutsaftakis@gmail.com>
2022-11-10 22:19:56 +0200

    fix: updated npm packages
    fixed typecheck
    fixed jest config
    fixed package-json types

--- jest.config.js
+++ jest.config.js
@@ -9,13 +9,15 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
   moduleFileExtensions: ['vue', 'js', 'ts'],
   moduleNameMapper: {
-    // Fix in order for vue-test-utils to work with Jest 28
-    // https://github.com/vuejs/vue-test-utils/issues/1975
-    '^@vue/test-utils$': require.resolve('@vue/test-utils'),
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@playground/(.*)$': '<rootDir>/playground/$1',
     '^@root/(.*)$': '<rootDir>/$1'
   },
   coveragePathIgnorePatterns: ['/node_modules/', '/playground/', '/tests/'],
-  coverageReporters: ['text', 'json-summary']
+  coverageReporters: ['text', 'json-summary'],
+  // Fix in order for vue-test-utils to work with Jest 28
+  // https://github.com/vuejs/vue-jest/issues/479
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons']
+  }
 }
